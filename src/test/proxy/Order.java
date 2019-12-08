c20f15411ff978c6ab943b34b9987439d65861ca
zhangyang <690196784@qq.com>
2019-12-08 23:24:48 +0800

    新增代码实现代理模式

---
+++ src/test/proxy/Order.java
@@ -0,0 +1,36 @@
+package test.proxy;
+
+/**
+ * 用静态代理的方式实现订单新增
+ * @author yang
+ * @Date  12-08 思路分析：
+ * 1 创建订单实体类 = 》 创建下发实际操作数据库的实体类  =》 service层接口隔离  =》 接口实现 
+ *    =》 动态依据订单创建时间按照年份进行分库 =》 创建切换数据源的代理类
+ */
+public class Order {
+	
+	private Object orderInfo;
+	private long createTime;
+	private String id;
+	public Object getOrderInfo() {
+		return orderInfo;
+	}
+	public void setOrderInfo(Object orderInfo) {
+		this.orderInfo = orderInfo;
+	}
+	public long getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(long createTime) {
+		this.createTime = createTime;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+} 
---
+++ src/test/proxy/Test.java
@@ -0,0 +1,28 @@
+package test.proxy;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import test.proxy.service.OrderService;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Order order = new Order();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Date date;
+		try {
+			date = simpleDateFormat.parse("2019/09/08");
+			order.setCreateTime(date.getTime());
+			OrderService orderService =  new OrderServiceStaticProxy(new OrderServiceImpl());
+			orderService.createOrder(order);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
