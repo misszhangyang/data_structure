80814afb4558e08f44111348492a3c20f89bcd64
“MissZY123” <18855990812@139.com>
2020-06-22 10:51:28 +0800

    0620号练习题

--- src/study/algorithm/0619/Dync0619.java
+++ src/study/algorithm/0619/Dync0619.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -9,36 +12,70 @@ import java.util.Stack;
  * 输出: true
  * 输入: "race a car"
  * 输出: false
- *
+ * <p>
  * 0619算法题02
  * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
  * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
  * 满足要求的三元组集合为：
  * [
- *   [-1, 0, 1],
- *   [-1, -1, 2]
+ * [-1, 0, 1],
+ * [-1, -1, 2]
  * ]
-  */
+ */
 public class Dync0619 {
 
+    public static List<int[]> getNum(int[] arr, int sum) {
+        List<int[]> listArr = new ArrayList<>();
+        int itemSUm = 0;
+        for (int i = 0; i < arr.length; i++) {
+            itemSUm = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                int secSUm = arr[j];
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (secSUm + arr[k] + itemSUm == sum) {
+                        int[] arr3 = new int[3];
+                        arr3[0] = i;
+                        arr3[1] = j;
+                        arr3[2] = k;
+                        listArr.add(arr3);
+                    }
+                }
+            }
+        }
+        return listArr;
+    }
+
+    public List<int[]> getListSum(int[] arr,int total){
+
+        return null;
+    }
+
+    public boolean isSUm(int[] arr, int index, int sum) {
+       return  arr[index] == sum;
+    }
 
 
     public static void main(String[] args) {
-        String str = "qwe8rertrer8ew q";
-        String newStr = str.replace(" ","");
-        System.out.println(isHuiWen(newStr));
+        int[] arr = {1, 0, -1, 2, -4, 2};
+        List<int[]> list = getNum(arr, 4);
+        for (int[] ints : list) {
+            System.out.println(Arrays.toString(ints));
+        }
+//        String str = "qwe8rertrer8ew q";
+//        String newStr = str.replace(" ","");
+//        System.out.println(isHuiWen(newStr));
     }
 
-    private static boolean isHuiWen(String str){
+    private static boolean isHuiWen(String str) {
         String[] strArr = str.split("");
         Stack<String> stack = new Stack<>();
-        for(String item : strArr){
+        for (String item : strArr) {
             stack.push(item);
         }
         boolean huiwen = true;
-        for(String item : strArr){
+        for (String item : strArr) {
             String itemStr = stack.pop();
-            if(!item.equals(itemStr)){
+            if (!item.equals(itemStr)) {
                 huiwen = false;
             }
         }
