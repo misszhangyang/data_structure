7b904bb6370782be5af2f7db9050e94887447cbe
“MissZY123” <18855990812@139.com>
2020-11-03 08:47:57 +0800

    二叉树相关知识点回顾

--- src/study/dataStru/binarytree/SortBinaryTree.java
+++ src/study/dataStru/binarytree/SortBinaryTree.java
@@ -13,6 +13,14 @@ package study.dataStru.binarytree;
  * 3 高度差大于1的时候开始进行旋转操作
  * 4 在旋转的时候需要依据实际的情况分清楚是进行左旋转还是进行右旋转
  * 5 旋转的时候需要进行判空的操作，在旋转完成之后需要进行return操作，避免出现浪费资源的程序判断
+ *
+ * 二叉排序树：中序遍历的时候从小到达的方式排列，即左边节点的
+ * value小于节点value，右边节点的value大于节点value
+ * 平衡二叉树：在满足二叉排序树的基础条件上，又要满足左右节点的高度差不能大于1
+ * 多叉树：  每个节点的存储的value不止一个，eg；右2-3树，b树，b+树等
+ * 2-3树： 每个节点最多存储2-3个value,且是从小到大排列
+ * b树： 每个节点可以存储多个value，且从小到大排列
+ * B+树：只有最低层的叶子节点会存储value值，子节点上只存储一般value作为索引，所以它相比b树最大的优势是可以更好的支持范围查询
  */
 public class SortBinaryTree {
 
@@ -25,6 +33,10 @@ public class SortBinaryTree {
             beTree.initBtree(node);
         }
         beTree.infoBetween();
+        System.out.println("--------------------------");
+        System.out.println(beTree.calRightHeight());
+        System.out.println("开始计算这棵树的高度差");
+        System.out.println(beTree.isOverHeight());
 
     }
 
@@ -48,6 +60,39 @@ class BeTree{
      */
     public void infoBetween(){
         this.root.inforBet();
+    }
+
+    /**
+     * 计算这棵树的右节点的高度
+     * @return
+     */
+    public int calRightHeight(){
+        return this.root.calRightHeight(this.root);
+    }
+
+    /**
+     * 统计这棵树的左节点的高度
+     * @return
+     */
+    public int calLeftHeight(){
+        return this.root.caLeftHeight(this.root);
+    }
+
+    /**
+     * 判断左右节点树的高度差大于1的各种情况
+     * 0 => 高度差没有大于1
+     * -1 => 左子树的高度差比右子树的高
+     * 1 => 右子树的高度比左子树的高
+     * @return
+     */
+    public int isOverHeight(){
+        if(calLeftHeight() - calRightHeight() > 1){
+            return -1;
+        }
+        if(calRightHeight() - calLeftHeight() > 1){
+            return -1;
+        }
+        return 0;
     }
 
 }
@@ -105,5 +150,27 @@ class Node {
     }
 
 
-    public void
+    /**
+     * 计算左子树的的高度
+     * @param node
+     * @return
+     */
+    public int caLeftHeight(Node node){
+        if(node.left != null){
+            return node.left.caLeftHeight(node.left) + 1;
+        }
+        return 1;
+    }
+
+    /**
+     * 计算右子树的的高度
+     * @param node
+     * @return
+     */
+    public int calRightHeight(Node node){
+        if(node.right != null){
+            return node.right.calRightHeight(node.right) + 1;
+        }
+        return 1;
+    }
 }
